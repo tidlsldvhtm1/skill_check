288d9601febbf3ebfabd3243af1275ee2bd70928
tidlsldvhtm1 <ww2_k@naver.com>
2021-04-22 01:36:51 +0900

    20210422 python3 level1

--- level1.py
+++ level1.py
@@ -1,22 +1,15 @@
 def solution(num):
     count = 0
-    num = int(input())
-
-    while count<500 :
-        if num%2==0 :
+    while count < 500 :
+        if num == 1 :
+            break
+        if num % 2 == 0 :
             num = num/2
-            count= count +1
-            if num == 1 :
-                break
-   
-        elif num%2==1 :
+            count = count + 1
+        
+        elif num % 2 == 1 :
             num = num * 3 + 1
-            count= count +1
-            if num == 1 :
-                break
-        
-        
-        
+            count = count + 1
     if count == 500 :
         answer = -1
     else :
